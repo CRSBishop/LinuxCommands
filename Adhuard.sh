e5355e66dcfbe86b61fcf1be381eded2bed1ec4f
CRSBishop <114109721+CRSBishop@users.noreply.github.com>
2022-11-08 18:17:30 +0000

    Update Adhuard.sh

--- Adhuard.sh
+++ Adhuard.sh
@@ -1,7 +1,7 @@
 docker run --name adguardhome\
     --restart unless-stopped\
-    -v /my/own/workdir:/opt/adguardhome/work\
-    -v /my/own/confdir:/opt/adguardhome/conf\
+    -v /home/chris/Adhuard/work:/opt/adguardhome/work\
+    -v /home/chris/Adguard/conf:/opt/adguardhome/conf\
     -p 53:53/tcp -p 53:53/udp\
     -p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
     -p 853:853/tcp\
