055f39a2ab20c90dd91147be9ad20f0d9d2a19c7
CRSBishop <114109721+CRSBishop@users.noreply.github.com>
2022-11-26 15:52:51 +0000

    Update Openhab.sh

--- Openhab.sh
+++ Openhab.sh
@@ -3,9 +3,12 @@ docker run \
         --net=host \
         -v /etc/localtime:/etc/localtime:ro \
         -v /etc/timezone:/etc/timezone:ro \
-        -v openhab_addons:/openhab/addons \
-        -v openhab_conf:/openhab/conf \
-        -v openhab_userdata:/openhab/userdata \
+        -v /opt/openhab/conf:/openhab/conf \
+        -v /opt/openhab/userdata:/openhab/userdata \
+        -v /opt/openhab/addons:/openhab/addons \
         -d \
+        -e USER_ID=<1000> \
+        -e GROUP_ID=<9001> \
+        -e CRYPTO_POLICY=unlimited \
         --restart=always \
         openhab/openhab:latest
